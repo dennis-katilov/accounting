bb0975984aa62511253bb7fa594c75e8dbc1bd6f
dennis-katilov <168256747+dennis-katilov@users.noreply.github.com>
2024-07-18 17:19:38 +0300

    Add files via upload

---
+++ cust_accounting.cpp
@@ -0,0 +1,16 @@
+//////////////////////////////
+//    Katilov Dennis        //
+//////////////////////////////
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <limits>
+#include <algorithm>
+
+using namespace std;
+
+//prototype of the functions
+
